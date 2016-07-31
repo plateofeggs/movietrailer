4d1c6e799f5aa0843d364e7c0575ee70dc5c3b8e
Zach Caldwell <zcald016@gmail.com>
2016-07-31 12:33:18 -0400

    Delete movies.py
    Example file that is not needed for the final version

--- movies.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-class Movie:
-    def __init__(self, youtube_url):
-        self.trailer_url = youtube_url
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_url)
