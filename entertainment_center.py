c115e8f6b3e99fc7bfe250a155ce8ace9178a29e
Zach Caldwell <zcald016@gmail.com>
2016-07-31 12:44:40 -0400

    final version

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,75 @@
 import fresh_tomatoes
 import media
+import requests
 
-the_departed = media.Movie("The Departed",
-						   "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
-						   "http://ia.media-imdb.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_UX182_CR0,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=iojhqm0JTW4")
+# Use OMDb API to serve us some json movie info (http://www.omdbapi.com/)
+# Getting movie data for The Departed
+td = requests.get(
+    "http://www.omdbapi.com/?t=The+Departed&y=&plot=short&r=json")
+td_data = td.json()
 
-threehundred = media.Movie("300",
-						   "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.",
-						   "http://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_UX182_CR0,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=UrIbxk7idYA")
+# Getting movie data for 300
+th = requests.get(
+    "http://www.omdbapi.com/?t=300&y=&plot=short&r=json")
+th_data = th.json()
 
-interstellar = media.Movie("Interstellar",
-						   "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-						   "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
+# Getting movie data for Interstellar
+st = requests.get(
+    "http://www.omdbapi.com/?t=Interstellar&y=&plot=short&r=json")
+st_data = st.json()
 
-pulp_fiction = media.Movie("Pulp Fiction",
-						   "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-						   "http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+# Getting movie data for Pulp Fiction
+pf = requests.get(
+    "http://www.omdbapi.com/?t=Pulp+Fiction&y=&plot=short&r=json")
+pf_data = pf.json()
 
-the_dark_knight = media.Movie("The Dark Knight",
-						   "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
-						   "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=yrJL5JxEYIw")
+# Getting movie data for The Dark Knight
+tdk = requests.get(
+    "http://www.omdbapi.com/?t=The+Dark+Knight&y=&plot=short&r=json")
+tdk_data = tdk.json()
 
-deadpool = media.Movie("Deadpool",
-						   "A former Special Forces operative turned mercenary is subjected to a rogue experiment that leaves him with accelerated healing powers, adopting the alter ego Deadpool.",
-						   "http://ia.media-imdb.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_UY268_CR1,0,182,268_AL_.jpg",
-						   "https://www.youtube.com/watch?v=FyKWUTwSYAs&oref=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DFyKWUTwSYAs&has_verified=1")
+# Getting movie data for Deadpool
+dp = requests.get(
+    "http://www.omdbapi.com/?t=Deadpool&y=&plot=short&r=json")
+dp_data = dp.json()
 
-movies = [the_departed, threehundred, interstellar, pulp_fiction, the_dark_knight, deadpool]
-#fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
+# Create instances of class Movie that will be sent to fresh_tomatoes.py
+the_departed = media.Movie(td_data["Title"],
+                           td_data["Plot"],
+                           "http://ia.media-imdb.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=iojhqm0JTW4")
+
+
+threehundred = media.Movie(th_data["Title"],
+                           th_data["Plot"],
+                           "http://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=UrIbxk7idYA")
+
+
+interstellar = media.Movie(st_data["Title"],
+                           st_data["Plot"],
+                           "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
+
+
+pulp_fiction = media.Movie(pf_data["Title"],
+                           pf_data["Plot"],
+                           "http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+
+
+the_dark_knight = media.Movie(tdk_data["Title"],
+                              tdk_data["Plot"],
+                              "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
+                              "https://www.youtube.com/watch?v=yrJL5JxEYIw")
+
+
+deadpool = media.Movie(dp_data["Title"],
+                       dp_data["Plot"],
+                       "http://ia.media-imdb.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_UY268_CR1,0,182,268_AL_.jpg",  # NOQA
+                       "https://www.youtube.com/watch?v=FyKWUTwSYAs&oref=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DFyKWUTwSYAs&has_verified=1")  # NOQA
+
+# fresh_tomatoes.py will use this list to make our web page
+movies = [the_departed, threehundred, interstellar,
+          pulp_fiction, the_dark_knight, deadpool]
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,15 +1,20 @@
 import webbrowser
 
+
 class Movie():
-	""" This class provides a way to show movie related information """
-	
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    """ Movie is a class to show movie related information
 
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+    Attributes:
+        movie_title (str): sets the title of the movie
+        storyline (str): sets the plot of the movie
+        poster_image_url (str): sets the url of the movie's poster
+        trailer_youtube_url (str): sets the url of the movie's trailer
+    """
 
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def __init__(self,
+                 movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
